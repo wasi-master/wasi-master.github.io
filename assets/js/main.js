6576f91420a043f320f70008dec328098a633f09
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-11-30 18:35:20 +0600

    Optimize link and script tags

--- assets/js/main.js
+++ assets/js/main.js
@@ -195,22 +195,6 @@
     aos_init();
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1,
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1,
-  });
-
   // Disable Ctrl+S
   $(document).bind("keydown", "ctrl+s", function (e) {
     e.preventDefault();
